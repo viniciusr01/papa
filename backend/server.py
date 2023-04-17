0bef9402c799fb33d78930d737f7c09203a16ffc
Emanuella Ferraz <emanuellaferraz@ufmg.br>
2023-04-17 11:05:39 -0300

    Trying lib to do authentication with wso2. Test

--- backend/server.py
+++ backend/server.py
@@ -1,23 +1,14 @@
-from flask import Flask 
+from flask import Flask, request, redirect, session, url_for
 from flask_cors import CORS
-from flask import request
-from flask import redirect
+from flask.json import jsonify
 import json
 import warnings
 import contextlib
 import requests
 from urllib3.exceptions import InsecureRequestWarning
 
-
-#Import Biblioteca do OpenID Connect
-from oic.oic import Client
-from oic.utils.authn.client import CLIENT_AUTHN_METHOD
-from oic import rndstr
-from oic.utils.http_util import Redirect
-from oic.oic.message import ProviderConfigurationResponse
-from oic.oic.message import RegistrationResponse
-from oic.oic.message import AuthorizationResponse
-
+from oauthlib.oauth2 import WebApplicationClient
+from requests_oauthlib import OAuth2Session
 
 import sys
 sys.path.insert(0,"..")
@@ -49,101 +40,22 @@ from services.gitLab import GitLab
 IPA = FreeIPA(FREEIPA_DOMAIN, FREEIPA_ROOT_USERNAME, FREEIPA_ROOT_PASSWORD )
 GL  = GitLab(GITLAB_DOMAIN, GITLAB_ROOT_USERNAME, GITLAB_ROOT_PASSWORD)
 
-
-
 old_merge_environment_settings = requests.Session.merge_environment_settings
 
-#Criar objeto cliente
-client = Client(client_authn_method=CLIENT_AUTHN_METHOD)
+# This information is obtained upon registration of a new GitHub
+client_id = OAUTH_CLIENT_KEY
+client_secret = OAUTH_CLIENT_SECRET
+redirect_uri=  "http://localhost:5000/callback"
 
-@contextlib.contextmanager
-def no_ssl_verification():
-    opened_adapters = set()
-
-    def merge_environment_settings(self, url, proxies, stream, verify, cert):
-        # Verification happens only once per connection so we need to close
-        # all the opened adapters once we're done. Otherwise, the effects of
-        # verify=False persist beyond the end of this context manager.
-        opened_adapters.add(self.get_adapter(url))
-
-        settings = old_merge_environment_settings(self, url, proxies, stream, verify, cert)
-        settings['verify'] = False
-
-        return settings
-
-    requests.Session.merge_environment_settings = merge_environment_settings
-
-    try:
-        with warnings.catch_warnings():
-            warnings.simplefilter('ignore', InsecureRequestWarning)
-            yield
-    finally:
-        requests.Session.merge_environment_settings = old_merge_environment_settings
-
-        for adapter in opened_adapters:
-            try:
-                adapter.close()
-            except:
-                pass
-
-
-#Registrando o Cliente (RP)
-from oic.oic.message import ProviderConfigurationResponse
-
-# Informações padrões sobre os end-points do Provedor OpenID (OP - WSO2 IS)
-#client.handle_provider_config('https://150.164.10.89:9443/oauth2/token/.well-known/openid-configuration', ['issuer'])
-op_info = ProviderConfigurationResponse( 
-    version="1.0", 
-    issuer= "https://150.164.10.89:9443/",
-    authorization_endpoint="https://150.164.10.89:9443/oauth2/authorize",
-    token_endpoint="https://150.164.10.89:9443/oauth2/token",
-    jwks_uri= "https://150.164.10.89:9443/oauth2/jwks",
-    userinfo_endpoint= "https://150.164.10.89:9443/oauth2/userinfo",
-    revocation_endpoint= "https://150.164.10.89:9443/oauth2/revoke",
-    introspection_endpoint= "https://150.164.10.89:9443/oauth2/introspect",
-    end_session_endpoint= "https://150.164.10.89:9443/oidc/logout"
-    )
-
-client.handle_provider_config(op_info, op_info['issuer'])
-
-# Client ID e Client Secret são geradas pelo Provedor OpenID (OP - WSO2 IS)
-info = {"client_id": "######", 
-        "client_secret": "########"
-        }
-
-
-client_reg = RegistrationResponse(**info)
-
-client.store_registration_info(client_reg)
-
-
-
-# Após realizar o registro do cliente, é possível solicitar que o OP autentique usuários e obtenha informações sobre eles.
-# O Fluxo de Autenticação Utilizado é Authorization Code Grant, definido pela RFC 6749 do OAuth 2.0
-# Para mais informações sobre o fluxo de autenticação: https://tools.ietf.org/html/rfc6749#section-4.1
-
-# Nesta etapa, ao fim é gerado a URL a qual o usuário deve ser direcionado para realizar o Login.
-
-state = rndstr()
-nonce = rndstr()
-
-args = {
-    "client_id": client.client_id,
-    "response_type": ['code'], # Determina o fluxo de autorização OAuth 2.0 que será utilizado
-    "scope": ["openid email"], #Por padrão é inserido 'openid', mas também pode ser inserido informações a qual deseja ter do usuário, como exemplo, email.
-    "nonce": nonce, #É um valor de string usado para associar uma sessão de cliente a um token de ID e para mitigar ataques de repetição
-    "redirect_uri": ['http://localhost:5000/callback'], #URL que o Provedor OpenID deve retornar após autenticação ser realizada
-    "state": state #É utilizado para controlar as respostas às solicitações pendentes
-}
-
-auth_req = client.construct_AuthorizationRequest(request_args=args)
-login_url = auth_req.request(client.authorization_endpoint)
-
+scope = ['openid email']
+oauth = OAuth2Session(client_id, redirect_uri=redirect_uri, scope=scope)
+authorization_base_url= oauth.authorization_url('https://150.164.10.89:9443/oauth2/authorize')
+token_url = 'https://150.164.10.89:9443/oauth2/token'
 
 
 app = Flask("PAPA - Backend")
 CORS(app)
-
+app.run(debug=True)
 
 @app.route("/")
 def home():
@@ -151,52 +63,24 @@ def home():
 
 @app.route("/login", methods=['GET'])
 def login():
-    return login_url
-        
+    wso2 = OAuth2Session(client_id)
+    authorization_url, state = wso2.authorization_url(authorization_base_url)
 
-#Rota que trata informações retornadas pela provedor OpenID (OP - WSO2 IS)
+    # State is used to prevent CSRF, keep this for later.
+    session['oauth_state'] = state
+    print ('Please go to %s and authorize access.' % authorization_url)
+    return redirect(authorization_url)
+
 @app.route("/callback")
 def callback():
-   
-    # If you're in a WSGI environment
-    # Coleta as informações da URL que foram retornadas pelo Provedor OpenID (OP - WSO2 IS)
-    response = request.query_string
-  
+    wso2 = OAuth2Session(client_id, state=session['oauth_state'])
+    token = wso2.fetch_token(token_url, client_secret=client_secret,
+                               authorization_response=request.url)
 
-    response = response.decode('ascii')
-   
-    
-    aresp = client.parse_response(AuthorizationResponse, info=response, sformat="urlencoded")
-    
-   
-    code = aresp["code"]
-    assert aresp["state"] == state   #Verifica se o state enviado na solicitação de autenticação é o mesmo retornado pelo Provedor Open ID (OP - WSO2 IS)
+    session['oauth_token'] = token
+    print (token)
+    return redirect('http://localhost:3000/home')
 
-  
-    with no_ssl_verification():
-        
-        # Utiliza o Code Grant Type retornado pelo OP para solicitar ao OP o Access Token e ID Token
-        args = {
-        "code": aresp["code"]
-        }
-        resp = client.do_access_token_request(state=aresp["state"],
-                                            request_args=args,
-                                            authn_method="client_secret_basic")
-
-    # Printa as informações sobre a autenticação realizada.
-    # Informações como: Access Token, ID Token, Token Type, entre outras.
-    print(resp)
-
-
-    if(resp["access_token"]!= ""):
-        return redirect("http://localhost:3000/home")
-    
-    else:
-        return "ERRO DE AUTENTICAÇÃO"
-
-
-
-app.run(debug=True)
 
 
 @app.route("/user", methods= ['GET', 'POST', 'PUT', 'DELETE'])
